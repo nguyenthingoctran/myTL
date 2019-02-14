abc2a86e12dee90dee407fe889d5299bd0054733
nguyenthingoctran <ngoctran2841@gmail.com>
2019-02-14 17:27:09 +0700

    14/2/2019 cty

--- demoFrontent/urls.py
+++ demoFrontent/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
+# Path master ("home/")
+
 app_name = 'demoFrontent'
 
 urlpatterns = [
-    url(r'^table/demo$', views.list_table)
+    url(r'^table/demo$', views.list_table),
+    url(r'^copy-to-clipboard$', views.copy_to_clipboard)
 ]
--- demoFrontent/views.py
+++ demoFrontent/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # SHOW DATA LIST TABLE
 def list_table(request):
     return render(request, 'demoFrontent/table_demo/list_table.html')
+
+def copy_to_clipboard(request):
+    return render(request, 'demoFrontent/page_copy_to_clipboard.html')
